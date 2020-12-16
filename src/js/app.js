5f32b49b5acbd1f8a9514a186afc7387e1149cec
xenolito <oscar.rey.tajes@gmail.com>
2020-12-16 10:58:13 +0100

    added highway.js for page load

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,9 @@
 import firma from "console-signature";
+import Highway from "@dogstudio/highway";
 
+// Call Highway.Core once.
+// Store it in a variable to use events
+const H = new Highway.Core();
 firma({
   author: "Aisling",
   url: "www.aisling.com",
